faa32e4ea1c1355538c3a06cdd0fbd6ae594a6e2
adamhardison284-pixel <adamhardison284@gmail.com>
2025-11-09 19:56:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import os
+from supabase import create_client, Client
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 def main():
     chrome_bin = os.getenv("CHROME_BIN", "/usr/bin/chromium-browser")
     chromedriver_path = os.getenv("CHROMEDRIVER_PATH", "/usr/bin/chromedriver")
-
+    
+    url = "https://jdnmanfimzvbilacjgcj.supabase.co"
+    key = "sb_secret_eVYWCtpPzmFsbJryaEug0A_EYBBcCII"
+    supabase: Client = create_client(url, key)	
+    
+    response = supabase.table("gmail_smtps").select("*").execute()
+    smtps = response.data
+    
     options = Options()
     options.binary_location = chrome_bin
     options.add_argument("--headless")
@@ -15,10 +26,24 @@ def main():
 
     driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
     url = "https://sprintmail.ru/"
-    driver.get(url)
-    print("Title:", driver.title)
-    driver.save_screenshot("screenshot.png")
-    driver.quit()
+    for smtp in smtps:
+    	if smtp['ready'] == True:
+            driver.get(url)
+            thus = True
+            while thus:
+                user_inp = driver.find_elements(By.ID, "webmail_username")
+                user_pass = driver.find_elements(By.ID, "webmail_pass")
+                user_inp.send_keys(smtp['username'])
+                user_pass.send_keys("Arbinaji1987$", Keys.ENTER)
+                time.sleep(3)
+                thus = False
+                current_url = driver.current_url
+                if "roundcubemail/?_task=mail&_mbox=INBOX" in current_url:
+                    response_data_ = supabase.table('gmail_smtps').update({"ready": 1, "checked": 1}).eq("id", smtp['id']).execute()
+                    driver.execute_script("document.getElementsByClassName('logout')[0].click()")
+                    time.sleep(2)
+                else:
+                    response_data_ = supabase.table('gmail_smtps').update({"ready": 0}).eq("id", smtp['id']).execute()
 
 if __name__ == "__main__":
     main()
