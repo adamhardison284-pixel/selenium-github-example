222cdfe53b5c8bbd7f74fb4bf6f16cf76f2edb64
adamhardison284-pixel <adamhardison284@gmail.com>
2025-11-22 19:33:18 +0100

    Update yahoo.py

--- yahoo.py
+++ yahoo.py
@@ -49,7 +49,7 @@ def main():
                         link.click()
                         time.sleep(2)
                         break       
-                table_name = "yahoo_de"
+                table_name = "yahoo_de_duplicate"
                 while True:
                     response_data_3 = supabase.rpc(
                         "select_before_and_after_update_yahoo",
